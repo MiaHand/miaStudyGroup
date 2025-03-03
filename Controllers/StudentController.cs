d084194b15459903a123e792557dddd9736806a5
miale <miale@LAPTOP-2V8KGV6K>
2025-03-03 21:02:45 +0200

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace miaStudyGroup.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult TableStudents()
+        {
+            List<Models.StudentClass> students = new List<Models.StudentClass>();
+
+            students.Add(new Models.StudentClass { StudentNumber = "u04949872", Name = "Mia", Surname = "Hand", EmailAddress = "u04949872@tuks.co.za" });
+            students.Add(new Models.StudentClass { StudentNumber = "u22512188", Name = "Cheyenne", Surname = "Scheepers", EmailAddress = "u22512188@tuks.co.za" });
+            students.Add(new Models.StudentClass { StudentNumber = "u20434066", Name = "Rory", Surname = "Finbow", EmailAddress = "u20434066@tuks.co.za" });
+            students.Add(new Models.StudentClass { StudentNumber = "u21493180", Name = "Samantha", Surname = "van Es", EmailAddress = "u21493180@tuks.co.za" });
+            students.Add(new Models.StudentClass { StudentNumber = "u22507176", Name = "Dehan", Surname = "Kriel", EmailAddress = "u22507176@tuks.co.za" });
+
+            return View(students);
+        }
+    }
+}
---
+++ Models/StudentClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace miaStudyGroup.Models
+{
+    public class StudentClass
+    {
+        // First decorator
+        [Display(Name = "Student Number")]
+        public string StudentNumber { get; set; }
+
+        // Second decorator
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        // Third decorator
+        [Display(Name = "Surname")]
+        public string Surname { get; set; }
+
+        // Fourth decorator
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+    }
+}
